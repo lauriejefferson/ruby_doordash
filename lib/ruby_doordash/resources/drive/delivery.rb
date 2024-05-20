e626b8541d081e5dfc429af55878dd7338611e25
lauriejefferson <joyfulnoiseforyahshua@gmail.com>
2024-05-20 16:30:29 -0400

    added methods to drive delivery resource and drive_classic businessstore resource

--- lib/ruby_doordash/resources/drive/delivery.rb
+++ lib/ruby_doordash/resources/drive/delivery.rb
@@ -1,29 +1,29 @@
 module RubyDoordash
   module Drive
-  class DeliveryResource < Resource
-    def get_delivery(external_delivery_id:)
-      Delivery.new get_request("drive/v2/deliveries/#{external_delivery_id}").body
-    end
+    class DeliveryResource < Resource
+      def get_delivery(external_delivery_id:)
+        Delivery.new get_request("drive/v2/deliveries/#{external_delivery_id}").body
+      end
 
-    def create_quote(**quote)
-      Delivery.new post_request("drive/v2/quotes", body: quote).body
-    end
+      def create_quote(**quote)
+        Delivery.new post_request("drive/v2/quotes", body: quote).body
+      end
 
-    def accept_quote(external_delivery_id:, **accept_quote )
-      Delivery.new post_request("drive/v2/quotes/#{external_delivery_id}/accept", body: accept_quote).body
-    end
+      def accept_quote(external_delivery_id:, **accept_quote )
+        Delivery.new post_request("drive/v2/quotes/#{external_delivery_id}/accept", body: accept_quote).body
+      end
 
-    def create_delivery(**delivery)
-      Delivery.new post_request('drive/v2/deliveries', body: delivery)
-    end
+      def create_delivery(**delivery)
+        Delivery.new post_request('drive/v2/deliveries', body: delivery)
+      end
 
-    def update_delivery(external_delivery_id:, **delivery)
-      Delivery.new patch_request("drive/v2/deliveries/#{external_delivery_id}", body: delivery).body
-    end
+      def update_delivery(external_delivery_id:, **delivery)
+        Delivery.new patch_request("drive/v2/deliveries/#{external_delivery_id}", body: delivery).body
+      end
 
-    def cancel_delivery(external_delivery_id:)
-      Delivery.new put_request("drive/v2/deliveries/#{external_delivery_id}/cancel", body: {}).body
+      def cancel_delivery(external_delivery_id:)
+        Delivery.new put_request("drive/v2/deliveries/#{external_delivery_id}/cancel").body
+      end
     end
   end
-end
 end
--- lib/ruby_doordash/resources/drive_classic/business_store.rb
+++ lib/ruby_doordash/resources/drive_classic/business_store.rb
@@ -1,37 +1,37 @@
 module RubyDoordash
   module DriveClassic
-  class BusinessStoreResource < Resource
-   def get_business(external_business_id:)
-    Business.new get_request("developer/v1/businesses/#{external_business_id}").body
-   end
+    class BusinessStoreResource < Resource
+      def get_business(external_business_id:)
+        Business.new get_request("developer/v1/businesses/#{external_business_id}").body
+      end
 
-   def update_business(external_business_id: , **business)
-    Business.new update_request("developer/v1/businesses/#{external_business_id}", body: business).body
-   end
+      def update_business(external_business_id: , **business)
+        Business.new patch_request("developer/v1/businesses/#{external_business_id}", body: business).body
+      end
 
-   def create_business(**business)
-    Business.new post_request("developer/v1/businesses", body: business).body
-   end
+      def create_business(**business)
+        Business.new post_request("developer/v1/businesses", body: business).body
+      end
 
-   def list_businesses
-    Business.new get_request("developer/v1/businesses").body
-   end
+      def list_businesses()
+        Businesses.new get_request("developer/v1/businesses").body
+      end
 
-   def get_store(external_business_id:, external_store_id:)
-    Store.new get_request("developer/v1/businesses/#{external_business_id}/stores/#{external_store_id}").body
-   end
+      def get_store(external_business_id:, external_store_id:)
+        Store.new get_request("developer/v1/businesses/#{external_business_id}/stores/#{external_store_id}").body
+      end
 
-   def update_store(external_business_id:, external_store_id: )
-    Store.new patch_request("developer/v1/businesses/#{external_business_id}/stores/#{external_store_id}").body
-   end
+      def update_store(external_business_id:, external_store_id:, **store )
+        Store.new patch_request("developer/v1/businesses/#{external_business_id}/stores/#{external_store_id}", body: store).body
+      end
 
-   def create_store(external_business_id:, **store )
-    Store.new post_request("developer/v1/businesses/#{external_business_id}/stores", body: store).body
-   end
+      def create_store(external_business_id:, **store )
+        Store.new post_request("developer/v1/businesses/#{external_business_id}/stores", body: store).body
+      end
 
-   def list_stores(external_business_id: )
-    Store.new get_request("developer/v1/businesses/#{external_business_id}/stores").body
-   end
+      def list_stores(external_business_id: )
+        Store.new get_request("developer/v1/businesses/#{external_business_id}/stores").body
+      end
+    end
   end
-end
 end
